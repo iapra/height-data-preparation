1e31696eae871d2c0c656f2b62066f14a8c92c4f
Irène Apra <ireneapra@gmail.com>
2022-04-13 10:11:59 +0200

    cleaned main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-import os
 import numpy as np
 
 from height_grids_main import height_grids_main
@@ -9,55 +8,58 @@ from utils import get_png
 from config import LAS_INPUT_FOLDER, DATA_SPLIT_FOLDER, \
     DIR_ABS_HEIGHT_GEOTIFF, DIR_REL_HEIGHT_GEOTIFF
 
-# starting time
-start = time.time()
-
 # 1 # 
 ### UPDATE CONFIG FILE
 
 ######################## RELATIVE HEIGHT ################################
 # 2 #
 ### INDICATE THE LAS CLASSIFICATION YOU WANT
-classification = 6      # Refers to LAS classification rules
-
-# 3 #
+classification = 6
 ### INDICATE WHAT YOU NEED WITH TRUE/FALSE
-relative_height_main(get_sql_cmd = False, \
-    get_intermediate_new_z = False, get_new_las_from_txt = False,\
-    classification = classification, get_new_las_directly = False)
+get_sql_cmd = False
+get_intermediate_new_z = False
+get_new_las_from_txt = False
+get_new_las_directly = False
 
 ######################### INTERPOLATION #################################
-# 2 #
+# 3 #
 ### INDICATE A SINGLE MERGED POINT CLOUD OR A PATH TO CREATE IT
 las_merged = f"{LAS_INPUT_FOLDER}/merged.las"
-
-# 3 #
 ### ENTER WHAT INTERPOLATION YOU WANT WITH TRUE/FALSE
-height_grids_main(no_interp = False, idw = False, nn =False, \
-    las_merged = las_merged)
+no_interp = False
+idw = False
+nn = False
 
 ######################### DATA-SPLIT ###################################
-# 2 #
-### Enter the directory of split folder to visualise if necessary
-DATA_SPLIT_FOLDER_TO_VISUALIZE = DATA_SPLIT_FOLDER + "_3"
-
-# 3 #
+# 4 #
 # INDICATE WHAT YOU NEED WITH TRUE/FALSE
-data_splitter(create_split = False, visualise_split = False, \
-    DATA_SPLIT_FOLDER_TO_VISUALIZE = None)
+create_split = False 
+visualise_split = False
+# IF visualise_split == True, give a directory:
+DATA_SPLIT_FOLDER_TO_VISUALIZE = None
 
 ######################### CONVERT TIF TO PNG ###########################
-# 2 #
+# 5 #
 ### ENTER WHICH FOLDER(S) CONTAINING GEOTIFF IMAGES TO CONVERT & NODATA VALUE
-FOLDER_LIST = [f"{DIR_REL_HEIGHT_GEOTIFF}/no_interpolation", \
-    f"{DIR_REL_HEIGHT_GEOTIFF}/nn_interpolation", \
-        f"{DIR_REL_HEIGHT_GEOTIFF}/idw_interpolation"]
+FOLDER_LIST = []
 NODATA = -9999
-
-get_png(FOLDER_LIST, NODATA, dtype = np.uint8)
+dtype = np.uint8
 
 ########################################################################
 
-# end time
-end = time.time()
-print(f"Runtime of the program was {int((end - start)/60)} min, {(end - start)%60} sec.")
+if __name__ == "__main__":
+    # starting time
+    start = time.time()
+
+    relative_height_main(get_sql_cmd, get_intermediate_new_z, \
+        get_new_las_from_txt, classification, get_new_las_directly)
+
+    height_grids_main(no_interp, idw, nn, las_merged)
+
+    data_splitter(create_split, visualise_split, DATA_SPLIT_FOLDER_TO_VISUALIZE)
+    
+    get_png(FOLDER_LIST, NODATA, dtype)
+
+    # end time
+    end = time.time()
+    print(f"Finished all tasks in {int((end - start)/60)} min, {(end - start)%60} sec.")
